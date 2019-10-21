c33aaf9a5dea79c3e740dd057d9db76ee5ef7b60
VigneshwaranPalanisamy <joywithvicky@gmail.com>
2019-10-21 17:38:00 +0530

    Java configuration class file for Spring context configration.

---
+++ SpringSampleTwoAnnotations/src/com/sample/spring/annotations/SpringAnnotationConfig.java
@@ -0,0 +1,10 @@
+package com.sample.spring.annotations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.sample.spring.annotations")
+public class SpringAnnotationConfig {
+
+}
